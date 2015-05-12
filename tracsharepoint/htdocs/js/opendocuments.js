cafdf1f2a9427fa68c910c25999daffc47d730cc
Nick Piper <nick.piper@cgi.com>
2015-05-12 14:27:38 +0100

    Fix IE detection - was failing for IE11

--- tracsharepoint/htdocs/js/opendocuments.js
+++ tracsharepoint/htdocs/js/opendocuments.js
@@ -1,25 +1,16 @@
 $(function() {
 
-    // http://stackoverflow.com/questions/19999388/jquery-check-if-user-is-using-ie
-    function msieversion() {
-        var ua = window.navigator.userAgent;
-        var msie = ua.indexOf("MSIE ");
-	
-        if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))      // If Internet Explorer, return version number
-            return parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)));
-        else
-            return 0;
-	
-	return false;
-    }
-    
     $("a.officelaunchlink").click(function() {
-	if (msieversion() > 0) { 
-	    if (window.ActiveXObject) {
-		ed = new ActiveXObject('SharePoint.OpenDocuments.4');
-		if (ed) {
-		    ed.EditDocument(this.href);
-		}
+	var ua = window.navigator.userAgent;
+	window.console.log("Checking UA %s", ua);
+	if (~ua.indexOf('MSIE ') || ~ua.indexOf('Trident/')) {
+	    // http://stackoverflow.com/questions/19999388/jquery-check-if-user-is-using-ie
+	    window.console.log("Detected IE");
+	    window.console.log("ActiveXObject is %s", window.ActiveXObject);	    
+	    ed = new ActiveXObject('SharePoint.OpenDocuments.4');
+	    window.console.log("ActiveXObject: %s", ed);		
+	    if (ed) {
+		ed.EditDocument(this.href);
 	    }
 	} else {
 	    alert("This feature requires Windows, Internet Explorer, and Microsoft Office.")
