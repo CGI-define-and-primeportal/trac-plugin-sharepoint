cd06d6e9ff893748637d371eae4c56020c6355fe
Nick Piper <nick.piper@cgi.com>
2015-07-27 17:04:05 +0100

    Remove left over console logging

--- tracsharepoint/htdocs/js/opendocuments.js
+++ tracsharepoint/htdocs/js/opendocuments.js
@@ -2,13 +2,9 @@ $(function() {
 
     $("a.officelaunchlink").click(function() {
 	var ua = window.navigator.userAgent;
-	window.console.log("Checking UA %s", ua);
 	if (~ua.indexOf('MSIE ') || ~ua.indexOf('Trident/')) {
 	    // http://stackoverflow.com/questions/19999388/jquery-check-if-user-is-using-ie
-	    window.console.log("Detected IE");
-	    window.console.log("ActiveXObject is %s", window.ActiveXObject);	    
 	    ed = new ActiveXObject('SharePoint.OpenDocuments.4');
-	    window.console.log("ActiveXObject: %s", ed);		
 	    if (ed) {
 		ed.EditDocument(this.href);
 	    }
